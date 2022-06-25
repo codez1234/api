6007835543510ffd77c8bba31876900c7e11588d
codez1234 <shubham@codez.in>
2022-06-25 20:36:40 +0530

    modified methods in TblPushNotificationLog

--- notifications/models.py
+++ notifications/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django_mysql.models import EnumField, FixedCharField
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.conf import settings
 User = settings.AUTH_USER_MODEL
 
@@ -100,14 +100,26 @@ class TblPushNotificationLog(models.Model):
         auto_now_add=True, db_column="fld_created_datetime")
 
     def getDate(self):
+        suffix = {
+            "01": "st",
+            "02": "nd",
+            "03": "rd",
+        }
         notification_date = self.created_datetime.date()
-        # print(f'notification_date == {notification_date} and {datetime.now()}')
-        if notification_date == datetime.now().date():
-            notification_date = "Today"
-        return notification_date
+        suffix_key = notification_date.strftime("%d")
+        today = datetime.now().date()
+        if notification_date == today:
+            return "Today"
+        elif today - timedelta(days=1) == notification_date:
+            return "Yesterday"
+        date_in_str = notification_date.strftime("%dth%B,%Y")
+        if suffix_key in suffix.keys():
+            return date_in_str.replace("th", suffix.get(suffix_key))
+        return date_in_str
 
     def getTime(self):
-        return self.created_datetime.time().strftime("%H:%M")
+        # return self.created_datetime.time().strftime("%H:%M") # in 24hr format
+        return self.created_datetime.time().strftime("%I:%M %p")
 
     def __str__(self) -> str:
         return self.user_id.email
